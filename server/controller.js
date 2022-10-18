c663c43542d28e90ad48052cba9bcac0753678a9
Tyler Jensen <troubletimestwo97@gmail.com>
2022-10-18 13:29:41 -0600

    added plus and minus buttons

--- server/controller.js
+++ server/controller.js
@@ -34,5 +34,22 @@ module.exports = {
 
         movies.push(newMovie)
         res.status(200).send(movies)
+    },
+    updateMovie: (req, res) => {
+        let type = req.body.type
+        let id = req.params.id
+        // console.log(type + ' to ' + id)
+        let index = movies.findIndex(element => element.id === +id)
+
+
+        if(type === 'plus') {
+            movies[index].rating++
+            res.status(200).send(movies)
+        } else if (type === 'minus') {
+            movies[index].rating--
+            res.status(200).send(movies)
+        } else {
+            res.sendstatus(400)
+        }
     }
 }
--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const {getMovies, deleteMovie, createMovie} = require('./controller.js')
+const {getMovies, deleteMovie, createMovie, updateMovie} = require('./controller.js')
 
 
 //endpoints
 app.get('/api/movies', getMovies)
 app.delete('/api/movies/:id', deleteMovie)
 app.post('/api/movies', createMovie)
+app.put('/api/movies/:id', updateMovie)
 
 app.listen(4004, () => {
     console.log('Up on 4004!')
